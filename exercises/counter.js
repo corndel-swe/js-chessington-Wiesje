6845de673dcf8ee5bd0f7ec9122f70d6411af952
wiesje <wiesje.heerik@corndel.com>
2024-06-24 14:17:14 +0100

    Solves 2

--- exercises/counter.js
+++ exercises/counter.js
@@ -3,10 +3,26 @@ class Counter {
     constructor() {
         this.count = 0
     }
+
+    increment() {
+        this.count++
+    }
+
+    reset() {
+        this.count = 0
+    }
+
+    getCount() {
+        return this.count
+    }
 }
 
 // Replace `null` with an _instance_ of your counter
 const counterInstance = new Counter()
+counterInstance.increment()
+console.log(counterInstance.count)
+counterInstance.reset()
+console.log(counterInstance.getCount())
 
 // Please don't change the lines below!
 export { Counter, counterInstance }
